7914e01144eb1ea33dd5240496a2f9ba8bd42965
MrReptile360 <73424160+MrReptile360@users.noreply.github.com>
2021-10-16 15:53:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,14 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyD2i0hSqW1HNYZwye8jgM7r16B7X3dNl9E",
+  authDomain: "teams-3dcf8.firebaseapp.com",
+  databaseURL: "https://teams-3dcf8-default-rtdb.firebaseio.com",
+  projectId: "teams-3dcf8",
+  storageBucket: "teams-3dcf8.appspot.com",
+  messagingSenderId: "1004606137989",
+  appId: "1:1004606137989:web:994029998647db04bf2bec"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
